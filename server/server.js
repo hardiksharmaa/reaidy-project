1d76982cc20f28da6947e81bccc98d0b4a270cae
Hardik <hs489819@gmail.com>
2026-01-17 17:26:25 +0530

    backend setup

---
+++ server/server.js
@@ -0,0 +1,27 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import helmet from 'helmet';
+import morgan from 'morgan';
+import connectDB from './config/db.js';
+
+dotenv.config();
+connectDB();
+
+const app = express();
+
+app.use(express.json()); 
+app.use(helmet()); 
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+app.use(morgan("common"));
+app.use(cors());
+
+app.get('/', (req, res) => {
+  res.status(200).json({ message: 'E-commerce API is running...' });
+});
+
+const PORT = process.env.PORT || 5001;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
